bc2ee1aec3a430cfaef974331fec3094c7f43fc8
samofoke <sabataernestmofokeng@gmail.com>
2023-07-15 14:48:43 +0200

    looking at layout components with styled components

---
+++ src/components/left/left.js
@@ -0,0 +1,9 @@
+const LeftComponent = () => {
+  return (
+    <div>
+      <h1>This is my left component</h1>
+    </div>
+  );
+};
+
+export default LeftComponent;
---
+++ src/components/right/right.js
@@ -0,0 +1,9 @@
+const RightComponent = () => {
+  return (
+    <div>
+      <h1>This is the Right component</h1>
+    </div>
+  );
+};
+
+export default RightComponent;
